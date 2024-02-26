e6ae99bb624483e97962b9b0fa44eb53550c3b94
Hashir789 <m.hashir.malik1020@gmail.com>
2024-02-26 13:22:56 +0500

    Second

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#cwwj3fvo9&(8+yfv_tygfsj!^#@+fwpcjg7s9dl&_lqe$xkxj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
